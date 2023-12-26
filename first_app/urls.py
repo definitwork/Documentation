5dd37d3c302da6cc839e3f0a4eab12e46d761031
Banywick <android19377@gmail.com>
2023-12-26 13:59:10 +0300

    ready work

--- first_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import get_first_product, ProductAPIView
-
-urlpatterns = [
-    path('', get_first_product),
-    path('api/', ProductAPIView.as_view()),
-
-]

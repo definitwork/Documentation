87574d7d187909915ad5283d7823d9ff467aea62
Ko100fei <dobysh.k@gmail.com>
2023-12-31 00:20:33 +0300

    Изменения названия приложения (перевод на русский)

--- copyright_contacts/apps.py
+++ copyright_contacts/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CopyrightContactsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'copyright_contacts'
+    verbose_name = 'Авторские права и контакты'

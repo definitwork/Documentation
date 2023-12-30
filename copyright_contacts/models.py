b0bb4c4bf9e891fd38d2757bfc0cdead2041f930
Ko100fei <dobysh.k@gmail.com>
2023-12-31 02:29:19 +0300

    Разные варианты проверки данных в таблице Copyright

--- copyright_contacts/models.py
+++ copyright_contacts/models.py
@@ -3,26 +3,39 @@ from django.core.exceptions import ValidationError
 
 
 class Copyright(models.Model):
-    site_name = models.CharField(verbose_name='Название проекта', max_length=255)
-    owner_name = models.CharField(verbose_name='ФИО', max_length=255)
-    unp = models.CharField(verbose_name='УНП', max_length=9)
-    country = models.CharField(verbose_name='Страна', max_length=255)
-    postcode = models.CharField(verbose_name='Индекс', max_length=255)
-    city = models.CharField(verbose_name='Город', max_length=255)
-    street = models.CharField(verbose_name='Улица', max_length=255)
-    house = models.CharField(verbose_name='Дом', max_length=255)
+    site_name = models.CharField(verbose_name='Название проекта', max_length=20)  # Название проекта
+    owner_name = models.CharField(verbose_name='ФИО', max_length=100)  # ФИО или название компании-владельца сайта
+    unp = models.CharField(verbose_name='УНП', max_length=9)  # УНП
+    country = models.CharField(verbose_name='Страна', max_length=25)  # Страна
+    postcode = models.CharField(verbose_name='Индекс', max_length=6)  # индекс
+    city = models.CharField(verbose_name='Город', max_length=25)  # Город
+    street = models.CharField(verbose_name='Улица', max_length=50)  # Улица
+    house = models.CharField(verbose_name='Дом', max_length=5)  # Номер дома
 
     class Meta:
         verbose_name_plural = 'Авторские права'
         verbose_name = 'Авторское право'
         ordering = ['site_name']
 
+    # def save(self, *args, **kwargs):
+    #     print(Copyright.objects.count())
+    #     if Copyright.objects.count() < 2:
+    #         print('ошибка')
+    #         # raise ValidationError("Модель может иметь только одну запись.")
+    #     super().save(*args, **kwargs)
+
     def __str__(self):
         return self.site_name
 
     def clean(self):
+        if Copyright.objects.exists() and not Copyright.objects.values('site_name')[0]['site_name'] == self.site_name:
+            raise ValidationError('Таблица может содержать только одну запись')
+
         if len(self.unp) < 9 or not self.unp.isdigit():
             raise ValidationError({'unp': 'УНП должен состоять из 9 цифр.'})
+
+        if len(self.postcode) < 6 or not self.postcode.isdigit():
+            raise ValidationError({'postcode': 'Не верный формат'})
 
 # class Contacts(models.Model):
 #     contact_title =

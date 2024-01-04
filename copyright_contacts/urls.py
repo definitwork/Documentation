1964882b3f2c0a730512b5f7add79adb0a2be42b
Ko100fei <dobysh.k@gmail.com>
2024-01-04 17:39:09 +0300

    Шлифовка для отправки v2

--- copyright_contacts/urls.py
+++ copyright_contacts/urls.py
@@ -5,5 +5,5 @@ from .views import CombinedListAPIView, ContactsListAPIView, CombinedAPIView
 urlpatterns = [
     path('copyright/', CombinedListAPIView.as_view(), name='copyright'),
     path('contacts/', ContactsListAPIView.as_view(), name='contacts'),
-    path('copyright_and_contacts2', CombinedAPIView.as_view(), name='contacts'),
+    path('copyright_and_contacts/', CombinedAPIView.as_view(), name='contacts'),
 ]

3886af3a53602a619ce0996f7837dc38aee41031
Ko100fei <dobysh.k@gmail.com>
2024-01-04 17:46:46 +0300

    Шлифовка для отправки v6 URLS

--- copyright_contacts/urls.py
+++ copyright_contacts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import CombinedListAPIView, ContactsListAPIView, CombinedAPIView
 
 urlpatterns = [
-    path('copyright/', CombinedListAPIView.as_view(), name='copyright'),
-    path('contacts/', ContactsListAPIView.as_view(), name='contacts'),
-    path('copyright_and_contacts/', CombinedAPIView.as_view(), name='contacts'),
+    path('copyright/', CombinedListAPIView.as_view(), name='copyright'),  # Только Авторское право
+    path('contacts/', ContactsListAPIView.as_view(), name='contacts'),  # Только контакты
+    path('copyright_and_contacts/', CombinedAPIView.as_view(), name='contacts'),  # Авторское право и контакты
 ]

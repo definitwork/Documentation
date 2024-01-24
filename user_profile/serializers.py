271c33955a36737c6a28c8dae556118565bdc150
Igor Kartel <igor.v.kartel@gmail.com>
2024-01-24 15:35:24 +0300

    В приложении user_pofile на бэке в регистрацию пользователя добавлены получение и валидация чекбокса о пользовательского принятии соглашения

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -3,19 +3,24 @@ from rest_framework import serializers
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    email = serializers.CharField(required=True, allow_null=False)
     password = serializers.CharField(min_length=8, write_only=True)
     password2 = serializers.CharField(min_length=8, write_only=True)
-    email = serializers.CharField(required=True, allow_null=False)
+    accept_terms = serializers.BooleanField()
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2']
+        fields = ['username', 'email', 'password', 'password2', 'accept_terms']
 
     def validate_email(self, value):
         if not value:
             raise serializers.ValidationError('Это поле не может быть пустым')
         return value
 
+    def validate_accept_terms(self, value):
+        if not value or value == 0:
+            raise serializers.ValidationError('Вы должны принять условия пользовательского соглашения')
+        return value
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()

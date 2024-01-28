ea3b267589f3d12dbe48081b1a14d4a9d69fc457
Ko100fei <dobysh.k@gmail.com>
2024-01-28 19:29:13 +0300

    Добавлена библиотека drf-recaptcha 3 для работы с recaptcha. Внесены изменения в user_profile.serializers.py, settings.py. Доработан JS.

--- user_profile/views.py
+++ user_profile/views.py
@@ -19,7 +19,6 @@ from user_profile.serializers import UserRegistrationSerializer, LoginSerializer
 
 class UserRegistrationAPIView(APIView):
     def post(self, request):
-        print(request.data, '\n', '--------------------------------------------------------------------')
         serializer = UserRegistrationSerializer(data=request.data, context={"request": request})
         if serializer.is_valid(raise_exception=True):
             username = serializer.validated_data.get("username")
